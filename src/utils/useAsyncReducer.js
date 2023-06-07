ac6f0f398001a323701a3bf9313e7e5245430c3a
Alex H <alex.holder@bigfootds.com>
2023-06-07 19:59:50 +1000

    feat: rewrite useAsyncReducer for clarity

--- src/utils/useAsyncReducer.js
+++ src/utils/useAsyncReducer.js
@@ -1,7 +1,21 @@
 import { useState } from "react";
 
 export default function useAsyncReducer(reducer, initState) {
-    const [state, setState] = useState(initState),
-        dispatchState = async (action) => setState(await reducer(state, action));
+	// State within this hook is normal
+	// - state: stores the result of the reducer
+	// - setState: used by the dispatchState function to make reducer behave asychronously
+	let [state, setState] = useState(initState);
+	
+	// Passes the dispatched action to the reducer,
+	// runs the reducer, waits for a result,
+	// applies the result to local state.
+	const dispatchState = async (action) => {
+		const newState = await reducer(state, action);
+		setState(newState);
+	};
+
+	// Exposes these two bits for other components to use:
+	// - local state, which is kept in sync with the reducer state on line 13
+	// - dispatchState, which is used to asychronously run a reducer's actions
     return [state, dispatchState];
 };
