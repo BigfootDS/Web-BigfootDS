0e2bb0d2011d711fcba02c16e76fcdf30643e8a4
Alex <alex.holder@bigfootds.com>
2024-12-28 22:34:03 +1100

    feat: projects page implemented with cards

--- src/consts.ts
+++ src/consts.ts
@@ -4,13 +4,28 @@
 export const SITE_TITLE = 'BigfootDS';
 export const SITE_DESCRIPTION = 'Australian-made games and software.';
 
-export const data = {
-	ppvr: {
-		gameAbbreviation: "PPVR",
-		gameTitle: "Planet Protector VR",
-		coverArtFilePath: "",
-		coverArtAltText: "Planet Protector VR game trailer.",
-		releaseDate: 2017,
+export interface ProjectPlatformsData {
+	name: string;
+	url: string;
+}
+
+export interface ProjectData {
+	title: string;
+	platforms: ProjectPlatformsData[];
+	type: "game" | "package" | "app";
+	abbreviation: string;
+	tagline: string;
+	releaseYear?: string;
+	ytIframeString?: string;
+}
+
+export const dataGamesReleased: Array<ProjectData> = [
+	{
+		abbreviation: "PPVR",
+		title: "Planet Protector VR",
+		type: "game",
+		tagline:"Defend your city from alien invaders in this colorful virtual reality game! Aim, shoot, and protect your planet for as long as you can!",
+		releaseYear: "2017",
 		platforms: [
 			{
 				name: "SteamVR",
@@ -32,27 +47,113 @@ export const data = {
 		  title="Planet Protector VR trailer"
 		></iframe>
 	  </span>`
-	},
-	tbb: {
-		gameAbbreviation: "TBB",
-		gameTitle: "The Bestest Beehive",
-		coverArtFilePath: "/images/GameLogoSwitch-01.png",
-		coverArtAltText: "The Bestest Beehive game cover art.",
-		releaseDate: "TBA",
+	}
+];
+
+export const dataGamesWip: Array<ProjectData> = [
+	{
+		abbreviation: "PSIM",
+		title: "Patron Simulator",
+		type: "game",
+		tagline: "Become the ultimate patron of power in your own fantasy world! Build your gathering of followers, manipulate the mortal realm and grow your godhood to become the highest power in reality.",
+		releaseYear: "Late 2025",
+		platforms: [
+			{
+				name: "Steam",
+				url: "https://store.steampowered.com/app/1792550/Patron_Simulator/"
+			}
+		]
+	}
+];
+
+export const dataGamesBackburner: Array<ProjectData> = [
+	{
+		abbreviation: "BEES",
+		title: "The Bestest Beehive",
+		type: "game",
+		releaseYear: "Eventually!",
+		tagline: "Explore your way around infinite worlds full of flourishing flora, brilliant bees, and long-forgotten wonders. You & your friends can rebuild the world together while collecting & growing a massive variety of flora & bees!",
 		platforms: [
 			{
 				name: "Steam",
 				url: "https://store.steampowered.com/app/749460/The_Bestest_Beehive/"
-			}, 
+			}
+		]
+	},
+];
+
+
+export const dataPackagesReleased: Array<ProjectData> = [
+	{
+		abbreviation: "SUPE",
+		title: "SuperCamo",
+		type: "package",
+		tagline: "Camo-inspired ODM for NeDB, built specifically for BigfootDS' needs.",
+		releaseYear: "September 2024",
+		platforms: [
 			{
-				name: "Nintendo Switch",
-				url: ""
-			}, 
+				name: "Website",
+				url:"https://bigfootds.github.io/supercamo/"
+			},
 			{
-				name: "Xbox One",
-				url: ""
+				name: "GitHub",
+				url: "https://github.com/BigfootDS/supercamo"
+			},
+			{
+				name: "NPM",
+				url: "https://www.npmjs.com/package/@bigfootds/supercamo"
+			}
+		]
+	},
+	{
+		abbreviation: "USEM",
+		title:"Unity Semver Updater",
+		type:"package",
+		tagline: "NodeJS tool to update a Unity project's version to a specified format - typically the semver format.",
+		platforms: [
+			{
+				name: "GitHub",
+				url: "https://github.com/BigfootDS/unity-semver-updater"
+			},
+			{
+				name: "NPM",
+				url:"https://www.npmjs.com/package/@bigfootds/unity-semver-updater"
 			}
 		],
-		ytIframeString: ""
+		releaseYear: "July 2024"
+	}, 
+	{
+		abbreviation: "NPMC",
+		title:"NPM Compliance Helper",
+		type: "package",
+		tagline: "Tool to help generate legal & copyright notices about project dependencies.",
+		releaseYear: "May 2024",
+		platforms: [
+			{
+				name: "GitHub",
+				url: "https://github.com/BigfootDS/npm-compliance-helper"
+			},
+			{
+				name:"NPM",
+				url:"https://www.npmjs.com/package/@bigfootds/npm-compliance-helper"
+			}
+		]
 	}
-}
+];
+
+
+export const dataAppsWip: Array<ProjectData> = [
+	{
+		abbreviation: "SPOL",
+		title: "Sourcepool",
+		type: "app",
+		tagline: "TTRPG content management system, ready for you to self-host. Open-source project currently in early WIP stages.",
+		releaseYear: "Late 2025",
+		platforms: [
+			{
+				name:"GitHub",
+				url: "https://github.com/BigfootDS/Sourcepool"
+			}
+		]
+	}
+]
