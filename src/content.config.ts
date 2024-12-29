40c7de04657c13637d3593acca83f9003816036d
Alex <alex.holder@bigfootds.com>
2024-12-29 18:42:59 +1100

    feat: opengraph and article improvements

--- src/content.config.ts
+++ src/content.config.ts
@@ -9,7 +9,7 @@ const blog = defineCollection({
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
-		pubDate: z.coerce.date().optional(),
+		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
--- src/content/news/first-post.md
+++ src/content/news/first-post.md
@@ -1,7 +1,7 @@
 ---
-title: 'First post'
-description: 'Lorem ipsum dolor sit amet'
-pubDate: 'Jul 08 2022'
+title: 'Website Overhaul'
+description: 'Been a while, time for an update!'
+pubDate: 'Dec 29 2024'
 heroImage: '/blog-placeholder-3.jpg'
 ---
 
